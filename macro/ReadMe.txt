cd3b5d9bd8adca172940f26419a6de75b662b8ab
SeanMaster <gungsing@gmail.com>
2016-03-22 21:24:22 +0800

    example: ex2.c
    Add new macro: calloc, realloc.

--- macro/ReadMe.txt
+++ macro/ReadMe.txt
@@ -2,4 +2,5 @@ ex1.c -- 一個簡單的macro測試程式，主要測試macro展開後的樣子
         gcc -E ex1.c -o ex1_E.c
         gcc -E ex1.c -o ex1_E_DEBUG.c
 
-ex2.c -- malloc Macro. TODO: free macro
+ex2.c -- malloc|calloc|realloc Macro. 
+      TODO: free macro
--- macro/ex2.c
+++ macro/ex2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MALLOC(p, s)    \
     if(!((p)=malloc(s))) { \
@@ -7,11 +8,25 @@
         exit(EXIT_FAILURE); \
     }
 
+#define CALLOC(p, n, s) \
+    if(!((p) = calloc(n, s))) { \
+        fprintf(stderr, "Insufficient memory"); \
+        exit(EXIT_FAILURE); \
+    }
+
+#define REALLOC(p, s) \
+    if(!((p) = realloc(p, s))) { \
+        fprintf(stderr, "Insufficient memory"); \
+        exit(EXIT_FAILURE); \
+    }
+
 int main(int argc, char **argv)
 {
+    
+    printf("//example for malloc\n");
     int *pi;
     float *pf;
-
+    
     MALLOC(pi, sizeof(int));
     MALLOC(pf, sizeof(float));
 
@@ -22,6 +37,32 @@ int main(int argc, char **argv)
 
     free(pi);
     free(pf);
+
+    printf("\n//example for calloc\n");
+    int *qi;
+    int i;    
+    CALLOC(qi, 5, sizeof(int));
+
+    for(i=0;i<5;i++)
+        *(qi+i) = i;
+
+    for(i=0;i<5;i++)
+        printf("*(qi+%d) = %d\n", i, *(qi+i));
+
+    free(qi);
+
+    printf("\n//example for realloc\n");
+    char *str;
+    MALLOC(str, sizeof(char)*15);
+    strcpy(str, "123456789abcde");
+    printf("String=%s, Address=%p\n", str, str);
+   
+    REALLOC(str, 25);
+    strcat(str, ".com");
+    printf("String=%s, Address=%p\n", str, str);
+    
+    free(str);
+
 
     return 0;
 }
