d25f93bcaccf49b753d5ade0a0c71613ab64f26d
SeanMaster <gungsing@gmail.com>
2016-04-14 10:06:03 +0000

    upload example: two_sum
    Just dummy code, not done.

---
+++ leetcode/two_sum/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *twoSum(int *nums, int numsSize, int target)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+
+
+
+    return 0;
+}
