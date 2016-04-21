7faaae0b48e5e34377dcf3c5a2a20b740d509389
SeanMaster <gungsing@gmail.com>
2016-04-21 03:23:21 +0000

    example: topic/bit_count.c
    calc how many 1 bit in input number.

---
+++ topic/bit_count.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int bit_count_1(unsigned int n)
+{
+    int count = 0;
+    int i;
+#if 0     //bad method, can't dill with big number
+    for(i=0; i<32; i++) {
+        if(n & (1<<i)) {
+            count++;
+        }
+    }
+#else //shift input n ont bit.
+    for(; n; n>>=1) {
+        if(n & 1)
+            count++;
+    }
+#endif
+    
+    return count;
+}
+
+int main(int argc, char **argv)
+{
+    unsigned int ret;
+    unsigned int input;
+    
+    if(argc != 2) {
+        printf("Usage: %s number.\n", argv[0]);
+        return -1;
+    }
+    
+    input = atoi(argv[1]);
+    ret = bit_count_1(input);
+    
+    printf("ret = %d\n", ret);
+    return 0;
+}
