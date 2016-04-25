c83ff54b6bc3674a28de3d5f51f837b2443dd4ea
SeanMaster <gungsing@gmail.com>
2016-04-25 02:12:11 +0000

    example: topic/year.c
    閏年判斷程式

---
+++ topic/year.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    while(scanf("%d", &year) != EOF) {
+        if(((year % 4 == 0) && (year % 100 != 0)) ||
+            (year % 400 == 0))
+            printf("Yes\n");
+        else
+            printf("No\n");
+    }
+
+
+
+    return 0;
+}
