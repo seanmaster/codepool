91ba925fb479091080041d1fe1435ca72e81794c
SeanMaster <gungsing@gmail.com>
2017-01-11 18:00:47 -0800

    Fix leap year wrong problem.

--- topic/year.c
+++ topic/year.c
@@ -5,7 +5,7 @@ int main()
     int year;
 
     while(scanf("%d", &year) != EOF) {
-        if(((year % 4 == 0) && (year % 100 != 0)) ||
+        if((year % 4 == 0) && (year % 100 != 0) ||
             (year % 400 == 0))
             printf("Yes\n");
         else
